0e1711a03f1d3843d699fcd3b10479bc37a9f4ef
mithunpaul08 <mithunpaul08@gmail.com>
2018-10-30 12:20:33 -0700

    reads till stanford collapsedw

--- L-biLSTM_2/dep_parsing.py
+++ L-biLSTM_2/dep_parsing.py
@@ -1,14 +1,18 @@
 from processors import ProcessorsBaseAPI
 
-self.API = ProcessorsBaseAPI(hostname="127.0.0.1", port=8886, keep_alive=True)
-doc2 = API.fastnlp.annotate(body)
-logger.debug(doc2)
+API = ProcessorsBaseAPI(hostname="127.0.0.1", port=8886, keep_alive=True)
 
 body="this is a good project"
 x=API.fastnlp.annotate(body)
 
 for s in x.sentences:
-    print(s.graphs["stanford-collapsed"]
-    dest = edges["destination"]
-    src = edges["source"]
-    rel = edges["relation"]
+    g=s.graphs
+    st=g["stanford-collapsed"]
+    print(st)
+    #edges=st['edges']
+    #print(edges)
+
+    #print(edges)
+    #dest = edges["destination"]
+    #src = edges["source"]
+    #rel = edges["relation"]
