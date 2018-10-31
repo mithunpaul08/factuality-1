3a04d41df7b861c2c5bbb74b89d25e1638ad69d1
mithunpaul08 <mithunpaul08@gmail.com>
2018-10-31 14:43:59 -0700

    dependency parsing workswq

--- L-biLSTM_2/dep_parsing.py
+++ L-biLSTM_2/dep_parsing.py
@@ -1,18 +1,32 @@
 from processors import ProcessorsBaseAPI
-
+import sys
 API = ProcessorsBaseAPI(hostname="127.0.0.1", port=8886, keep_alive=True)
 
-body="this is a good project"
-x=API.fastnlp.annotate(body)
+body="he can not remember leaving"
+data=API.fastnlp.annotate(body)
 
-for s in x.sentences:
+
+#dependencies = [s['graphs']['stanford-collapsed'] for s in data.sentences]
+#print(dependencies)
+
+for s in data.sentences:
+    # play around with the dependencies directly
+    deps = s.dependencies
+
+    # see what dependencies lead directly to the first token (i.e. token 0 is the dependent of what?)
+    print(deps.incoming[0])
+
+    # see what dependencies are originating from the first token (i.e. token 0 is the head of what?)
+    print(deps.outgoing[3])
+    sys.exit(1)
+
+
     g=s.graphs
     st=g["stanford-collapsed"]
     print(st)
-    #edges=st['edges']
-    #print(edges)
-
-    #print(edges)
+    edges=st.edges
+    print(edges[0]["destination"])
     #dest = edges["destination"]
-    #src = edges["source"]
+    #print(dest)    
+#src = edges["source"]
     #rel = edges["relation"]
